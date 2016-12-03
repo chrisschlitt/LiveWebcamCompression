667c98a41e9aaa1cc819fd11885f854ed1b05c43
David Piccolella <davepiccolella@gmail.com>
2016-12-03 15:17:03 -0500

    Adding main menu

--- WebcamPictures.java
+++ WebcamPictures.java
@@ -4,31 +4,51 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.*;
-import java.awt.Graphics2D;
+
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.WebcamResolution;
 
 public class WebcamPictures {
 	public static void main(String[] args) {
+		
 		Webcam webcam = Webcam.getDefault();
 		webcam.setViewSize(WebcamResolution.VGA.getSize());
-
-		WebcamPanel panel = new WebcamPanel(webcam);
-		panel.setFPSDisplayed(true);
-		panel.setDisplayDebugInfo(true);
-		panel.setImageSizeDisplayed(true);
-		panel.setMirrored(true);
-		
 		JFrame frame = new JFrame("Test");
-		JPanel buttonPanel = new JPanel();
-		JButton showDialogButton = new JButton("Take Picture");
+		JPanel mainMenu = new JPanel(new GridLayout(0,1));
+		JPanel top = new JPanel(new GridLayout(0,1));
+		JLabel welcome = new JLabel("Welcome to the Webcam App!", SwingConstants.CENTER);
+		JLabel details = new JLabel("Choose server to broadcast or client to receive video", SwingConstants.CENTER);
 		
-		class takePicture implements ActionListener {
+		
+		
+		Font font = new Font("Courier", Font.BOLD,36);
+		welcome.setFont(font);
+		font = new Font("Courier", Font.PLAIN, 20);
+		details.setFont(font);
+		top.add(welcome, BorderLayout.NORTH);
+		top.add(details, BorderLayout.SOUTH);
+		
+		JButton serverButton = new JButton("Server");
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(serverButton, BorderLayout.EAST);
+		JButton clientButton = new JButton("Client");
+		buttonPanel.add(clientButton, BorderLayout.WEST);
+		mainMenu.add(top, BorderLayout.NORTH);
+		mainMenu.add(buttonPanel, BorderLayout.SOUTH);
+		frame.add(mainMenu, BorderLayout.CENTER);
+		frame.setResizable(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.pack();
+		frame.setSize(640, 557);
+		frame.setVisible(true);
+		
+		
+		class TakePicture implements ActionListener {
 			private int numPictures;
-			takePicture() {
+			
+			public TakePicture() {
 				numPictures = 1;
 			}
 			public void actionPerformed(ActionEvent e) {
@@ -38,7 +58,7 @@ public class WebcamPictures {
 				int b = 0;
 				BufferedImage image = webcam.getImage();
 				BufferedImage reconstructed = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-		
+
 				try {
 					ImageIO.write(image, "PNG", new File("test"+numPictures+".png"));
 					int[] imageColors = image.getRGB(0, 0, image.getWidth(), image.getHeight(), null, 0, image.getWidth());
@@ -53,7 +73,6 @@ public class WebcamPictures {
 							x = 0;
 							y++;
 						}
-						
 						color = new Color(imageColors[count]);
 						r = color.getRed();
 						g = color.getGreen();
@@ -71,13 +90,53 @@ public class WebcamPictures {
 			}
 		}
 		
-		showDialogButton.addActionListener(new takePicture());
-		buttonPanel.add(showDialogButton, BorderLayout.CENTER);
-		frame.add(buttonPanel, BorderLayout.SOUTH);
-		frame.add(panel, BorderLayout.NORTH);
-		frame.setResizable(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
+		class serverSelect implements ActionListener {
+			public void actionPerformed(ActionEvent ev) {
+				
+				frame.remove(mainMenu);
+				WebcamPanel panel = new WebcamPanel(webcam);
+				panel.setFPSDisplayed(false);
+				panel.setDisplayDebugInfo(false);
+				panel.setImageSizeDisplayed(false);
+				panel.setMirrored(true);
+				
+				JPanel buttonPanel = new JPanel();
+				JLabel label = new JLabel("Sending Video", JLabel.CENTER);
+				JButton takePictureButton = new JButton("Take Picture");
+				takePictureButton.addActionListener(new TakePicture());
+				buttonPanel.add(takePictureButton, BorderLayout.SOUTH);
+				frame.add(buttonPanel, BorderLayout.SOUTH);
+				frame.add(panel, BorderLayout.CENTER);
+				frame.add(label, BorderLayout.NORTH);
+				frame.setResizable(true);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.pack();
+				frame.setVisible(true);
+			}
+		}
+		
+		class clientSelect implements ActionListener {
+			public void actionPerformed(ActionEvent ev) {
+				
+				frame.remove(mainMenu);
+				WebcamPanel panel = new WebcamPanel(webcam);
+				panel.setFPSDisplayed(false);
+				panel.setDisplayDebugInfo(false);
+				panel.setImageSizeDisplayed(false);
+				panel.setMirrored(true);
+				
+				JLabel label = new JLabel("Receiving Video", JLabel.CENTER);
+				frame.add(label, BorderLayout.NORTH);
+				frame.add(panel, BorderLayout.SOUTH);
+				frame.setResizable(true);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.pack();
+				frame.setVisible(true);
+				
+			}
+		}
+		serverButton.addActionListener(new serverSelect());
+		clientButton.addActionListener(new clientSelect());
+		
 	}
 }
